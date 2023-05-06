7c7c5142cfd0a347d17aaf549887ec362022fca7
Mawojast <inet.beck@gmail.com>
2023-05-06 03:27:36 +0200

    section16 - Interceptors: HTTP-Requests abfangen und transformieren

--- Chapter 3 - BookMonkey 5/book-monkey/src/app/app.module.ts
+++ Chapter 3 - BookMonkey 5/book-monkey/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 
+import { AuthInterceptor } from './shared/auth.interceptor';
+
+import { AppComponent } from './app.component';
 import { BooksModule } from './books/books.module';
 import { HomeComponent } from './home/home.component';
 import { SearchComponent } from './search/search.component';
@@ -21,7 +22,11 @@ import { SearchComponent } from './search/search.component';
     BooksModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [{
+    provide: HTTP_INTERCEPTORS,
+    useClass: AuthInterceptor,
+    multi: true
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
